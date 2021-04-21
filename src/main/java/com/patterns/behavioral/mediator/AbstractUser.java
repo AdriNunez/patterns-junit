955de57756c561f70470d190c6caac76ec47478d
Adrián Núñez <adrinunezmunoz@gmail.com>
2021-04-21 21:13:55 +0200

    Fixed AbstractUser test5 and getter.

--- src/main/java/com/patterns/behavioral/mediator/AbstractUser.java
+++ src/main/java/com/patterns/behavioral/mediator/AbstractUser.java
@@ -4,6 +4,7 @@ public abstract class AbstractUser {
 
     protected ChatMediator mediator;
     protected String name;
+    protected String message;
 
     public AbstractUser(ChatMediator mediator, String name) {
         this.mediator = mediator;
@@ -15,5 +16,8 @@ public abstract class AbstractUser {
 
     public abstract void receive(String message);
 
+    public String getMessage(){
+        return message;
+    }
 
 }
